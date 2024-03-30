92e78f54fc58bcea85b954711a5d6c356b5d5078
arteM19B <159503211+arteM19B@users.noreply.github.com>
2024-03-31 03:14:02 +1400

    Update B.slugs.cpp

--- DP by submasks and subsegments/B.slugs.cpp
+++ DP by submasks and subsegments/B.slugs.cpp
@@ -10,17 +10,17 @@ const ll INF = 1e18;
 int main() {
     int n;
     cin >> n;
-    vector<ll> a (n);
+    vector<ll> a(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    vector<ll> ps (n + 1);
+    vector<ll> ps(n + 1);
     ps[0] = 0;
-    for (int i = 1; i < n; i++) {
-        ps[i] = a[i - 1] + ps[i] - 1;
+    for (int i = 1; i <= n; i++) {
+        ps[i] = a[i - 1] + ps[i - 1];
     }
 
-    vector<vector<ll>> dp (n, vector<ll> (n));
+    vector<vector<ll>> dp(n, vector<ll> (n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == j) {
@@ -35,9 +35,10 @@ int main() {
         for (int L = 0; L + len - 1 < n; L++) {
             int R = L + len - 1;
             for (int m = L; m < R; m++) {
-                dp[L][R] = min(dp[L][R], dp[L][m] + dp[m + 1][R] + ps[R + 1] - ps[L - 1]);
+                dp[L][R] = min(dp[L][R], dp[L][m] + dp[m + 1][R] + ps[R + 1] - ps[L]);
             }
         }
     }
     cout << dp[0][n - 1];
+    return 0;
 }
